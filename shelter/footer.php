d9cc1173fdd658cfa56cb7fb4d5a7c066078acb4
GeorgeRyu <r.takahasii@gmail.com>
2018-07-20 18:49:15 -0700

    change footer image and modify footer layout

--- shelter/footer.php
+++ shelter/footer.php
@@ -1,11 +1,37 @@
-				<div class="section-bottom__footer">
-					<div class="footer-img">
-						<p class="copyright">@ 2017 shelter.Inc</p>				
+	<section id="contact" class="section section-bottom">
+
+		<div class="section-bottom__wrap">
+
+			<div class="section-bottom__ad-form">
+				<div class="section-bottom__wrap-address">
+					<div class="section-bottom__address">
+						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/logo-02.png" alt="logo">
+						<p>
+							スタジオシェルター　Shelter Inc<br>
+							410-3611 静岡県賀茂郡松崎町松崎 208-2
+						</p>
+						<div class="section-bottom__large-text">
+							<p class="tel">090-1558-1890
+							</p>
+							<p class="ad">shelterfly3@yahoo.co.jp</p>
+						</div>					
 					</div>
 				</div>
-
+				<div class="section-bottom__form">
+					<div class="section-bottom__wrap-form">
+						<h2 class="yellowtail">Contact</h2>
+						<?php echo do_shortcode('[contact-form-7 id="49" title="コンタクトフォーム 1"]'); ?>
+					</div>
+				</div>
 			</div>
-		</section>
+			<div class="section-bottom__footer">
+				<div class="footer-img">
+					<p class="copyright">@ 2017 shelter.Inc</p>				
+				</div>
+			</div>
+
+		</div>
+	</section>
 
 	<?php wp_footer(); ?>
 
--- shelter/front-page.php
+++ shelter/front-page.php
@@ -67,29 +67,4 @@
 		</div>					
 	</section>
 
-	<section id="contact" class="section section-bottom">
-
-		<div class="section-bottom__wrap">
-
-			<div class="section-bottom__ad-form">
-				<div class="section-bottom__address">
-					<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/logo-02.png" alt="logo">
-					<p>
-						スタジオシェルター　Shelter Inc<br>
-						410-3611 静岡県賀茂郡松崎町松崎 208-2
-					</p>
-					<div class="section-bottom__large-text">
-						<p class="tel">090-1558-1890
-						</p>
-						<p class="ad">shelterfly3@yahoo.co.jp</p>
-					</div>					
-				</div>
-				<div class="section-bottom__form">
-					<div class="section-bottom__wrap-form">
-						<h2>お問い合わせ</h2>
-						<?php echo do_shortcode('[contact-form-7 id="49" title="コンタクトフォーム 1"]'); ?>
-					</div>
-				</div>
-			</div>
-
 <?php get_footer(); ?>
